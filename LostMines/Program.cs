bbeb209fe0fdea5f2c56d3c9b22bb26996febea3
Brandon Smith <bsmith62598@outlook.com>
2021-02-10 21:07:18 -0600

    Updated TODO list

--- LostMines/Program.cs
+++ LostMines/Program.cs
@@ -10,7 +10,6 @@ namespace LostMines
     {
         static void Main(string[] args)
         {
-            //This is the program that will control the flow of the overall experience for the user. All calsses will be created in separate files and referred to in this program to allow us to use instances of monsters, players, weapons, etc.
             Console.Title = "Lost Mines of Averice";
             Console.WriteLine("Your journey into the mines begins...");
             int score = 0;
@@ -22,13 +21,10 @@ namespace LostMines
 
             do
             {
-                //Enter a room
-                //TODO 3. Call GetRoom()
                 Console.WriteLine(GetRoom());
 
                 //TODO 4. Create a monster for the room - learn about creating objects and then randomly selecting one.
 
-                //TODO 5. loop for the room
                 bool reload = false;
                 do
                 {
@@ -43,13 +39,10 @@ namespace LostMines
                         "Current Score: {0}\n\n", score);
                     #endregion
 
-                    //TODO 7. Capture user choice.
                     #region User Choice
                     ConsoleKey userChoice = Console.ReadKey(true).Key;
-                    //Above will execute upon input without having to hit enter
                     #endregion
 
-                    //TODO 8. Clear the console and build the switch based on userChoice
                     Console.Clear();
                     #region Switch that runs functionality based on user choice
                     switch (userChoice)
@@ -95,14 +88,10 @@ namespace LostMines
             } while (!exit);
         }
 
-        //TODO 16. Create a GetRoom() and plug it in to the todo above
-
         private static string GetRoom()
         {
-            //Create an array of room descriptions - string datatype
             string[] rooms =
             {
-                //Place a variety of room descriptions here
                 "A rusted portcullis stands just beyond the door. Looking into the room, you see three other exits, similarly blocked by portcullises. Four skeletons dressed in aged clothing and rusting armor lie on the floor in the room against the walls. They seem in poses of repose rather than violence.",
                 "This narrow room at first appears to be a dead-end corridor, but then you note several metal plates on the walls set at about eye height. Looking more closely, you see that one of these plates is slid aside to reveal a peephole",
                 " This chamber of well-laid stones holds a wide bas-relief of a pastoral scene. In it you see a mountain like Mount Waterdeep, except that Castle Waterdeep and the city are missing. Instead, a small fishing village is a short way from a walled complex with several tall towers.",
@@ -113,13 +102,12 @@ namespace LostMines
                 "A huge iron cage lies on its side in this room, and its gate rests open on the floor. A broken chain lies under the door, and the cage is on a rotting corpse that looks to be a hobgoblin. Another corpse lies a short distance away from the cage. It lacks a head.",
                 " A rusted portcullis stands just beyond the door. Looking into the room, you see three other exits, similarly blocked by portcullises. Four skeletons dressed in aged clothing and rusting armor lie on the floor in the room against the walls. They seem in poses of repose rather than violence."
             };
-            //Generate a Random object and get a random room description
+
             Random rand = new Random();
-            //since the max value in the Next() is exclusive, we can just use the .Length property to include all indexes from our array.
             int indexNbr = rand.Next(rooms.Length);
             string room = "**** NEW ROOM ****\n" + rooms[indexNbr] + "\n";
-            //Return room
             return room;
+
         }
     }
 }
